1fef691f558b0eb28e82d281accb69f300249dd8
Lori Ainslie <lori.ainslie18@gmail.com>
2021-09-27 10:19:19 -0400

    completed more SQL exercises

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -24,3 +24,25 @@ Query the list of CITY names from STATION that either do not start with vowels o
 select distinct city from station
 where city not regexp '^[aeiou]' or city not regexp '[aeiou]$';
 
+/*
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+*/
+
+select distinct city from station
+where city not rlike '^[aeiou]'  and city not rlike '[aeiou]$';
+
+/*
+Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+*/
+
+select name from students
+where marks > 75
+order by substr(name, -3), ID;
+
+/*
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+*/
+
+select name from employee
+order by name;
+
