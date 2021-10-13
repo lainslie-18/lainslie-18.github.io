359cf612adec27762a522a18be6ad969db5050ad
Lori Ainslie <lori.ainslie18@gmail.com>
2021-10-13 08:05:59 -0400

    added completed exercises

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -46,3 +46,30 @@ Write a query that prints a list of employee names (i.e.: the name attribute) fr
 select name from employee
 order by name;
 
+/*
+Generate the following two result sets:
+
+Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
+
+There are a total of [occupation_count] [occupation]s.
+
+where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
+
+Note: There will be at least two entries in the table for each type of occupation.
+*/
+
+select concat(name,
+    (case
+        when occupation = 'Actor' then '(A)'
+        when occupation = 'Doctor' then '(D)'
+        when occupation = 'Professor' then '(P)'
+        when occupation = 'Singer' then '(S)'
+        end)) as name
+from occupations
+order by name;
+
+select 'There are a total of', count(occupation), concat(lower(occupation), 's.') as occupation
+from occupations
+group by occupation
+order by count(occupation);
