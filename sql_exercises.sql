62d919209517238f2f2e2280abeed7ed1af6891c
Lori Ainslie <lori.ainslie18@gmail.com>
2021-12-24 18:33:24 -0500

    updating with additional completed exercises

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -86,3 +86,55 @@ months < 10;
 
 select count(name) from city
 where population > 100000;
+
+
+/*
+Query the total population of all cities in CITY where District is California.
+*/
+select sum(population) from city
+where district = 'California';
+
+/*
+Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+*/
+select * from city
+where countrycode = "USA"
+and population > 100000;
+
+/*
+Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345 Truncate your answer to 4 decimal places.
+*/
+select round(max(lat_n), 4) from station
+where lat_n < 137.2345;
+
+/*
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345 Round your answer to 4 decimal places.
+*/
+select round(long_w,4) from station 
+where lat_n = (select max(lat_n) from station
+                where lat_n < 137.2345);
+
+/*
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780 Round your answer to 4 decimal places.
+*/
+select round(min(lat_n),4) from station
+where lat_n > 38.7780;
+
+/*
+Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780 Round your answer to 4 decimal places.
+*/
+select round(long_w, 4) from station
+where lat_n = (select min(lat_n) from station
+                where lat_n > 38.7780);
+
+/*
+Consider P1(a,b) and P2(c,d) to be two points on a 2D plane.
+
+a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places.
+*/
+select round(max(lat_n) - min(lat_n) + max(long_w) - min(long_w), 4) 
+from station;
