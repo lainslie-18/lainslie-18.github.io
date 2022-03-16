a8d8facd3df53b59451fa8d9360e38cc3ffbe438
Lori Ainslie <lori.ainslie18@gmail.com>
2022-03-15 21:30:48 -0500

    updating with additional sql practice

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -138,3 +138,55 @@ Query the Manhattan Distance between points P1 and P2 and round it to a scale of
 */
 select round(max(lat_n) - min(lat_n) + max(long_w) - min(long_w), 4) 
 from station;
+
+/*
+Change the query shown so that it displays Nobel prizes for 1950.
+*/
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1950
+
+ /*
+ Show who won the 1962 prize for Literature.
+ */
+SELECT winner
+FROM nobel
+WHERE yr = 1962
+AND subject = 'Literature'
+
+ /*
+Show the year and subject that won 'Albert Einstein' his prize.
+ */
+SELECT yr, subject 
+FROM nobel
+WHERE winner = 'Albert Einstein'
+
+ /*
+Give the name of the 'Peace' winners since the year 2000, including 2000.
+ */
+SELECT winner
+FROM nobel
+WHERE subject = 'Peace'
+AND yr >= 2000
+
+ /*
+Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+ */
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Literature'
+AND yr BETWEEN 1980 AND 1989
+
+ /*
+Show all details of the presidential winners:
+
+Theodore Roosevelt
+Woodrow Wilson
+Jimmy Carter
+Barack Obama
+ */
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                    'Woodrow Wilson',
+                    'Jimmy Carter', 
+                    'Barack Obama')
