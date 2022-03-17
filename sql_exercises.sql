10f785fbedb49079c77d28331cb45d7b98fc920f
Lori Ainslie <lori.ainslie18@gmail.com>
2022-03-16 20:49:04 -0400

    updating with additional sql practice

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -190,3 +190,57 @@ SELECT * FROM nobel
                     'Woodrow Wilson',
                     'Jimmy Carter', 
                     'Barack Obama')
+
+ /*
+Show the winners with first name John
+ */
+SELECT winner FROM nobel
+WHERE winner LIKE 'John%'
+
+/*
+Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
+ */
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Physics' AND yr = '1980'
+OR subject = 'Chemistry' AND yr = '1984'
+
+/*
+Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
+ */
+SELECT yr, subject, winner FROM nobel
+WHERE yr = '1980' AND subject NOT IN ('Chemistry', 'Medicine')
+
+/*
+Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+ */
+SELECT yr, subject, winner FROM nobel
+WHERE subject = 'Medicine' AND yr < 1910
+OR subject = 'Literature' and yr >= 2004
+
+/*
+Find all details of the prize won by PETER GRÜNBERG
+ */
+ SELECT * FROM nobel
+WHERE winner = 'Peter Grünberg'
+
+ /*
+Find all details of the prize won by EUGENE O'NEILL
+ */
+SELECT * FROM nobel
+WHERE winner = 'Eugene O\'Neill'
+'
+ /*
+List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
+ */
+SELECT winner, yr, subject FROM nobel
+WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC, winner
+
+ /*
+Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+ */
+SELECT winner, subject
+FROM nobel
+WHERE yr=1984
+ORDER BY subject IN ('Physics', 'Chemistry'), subject, winner
