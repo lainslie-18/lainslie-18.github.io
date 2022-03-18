5fd3c224b2b7459753a126b42ef15fcb7e8e7294
Lori Ainslie <lori.ainslie18@gmail.com>
2022-03-17 23:02:55 -0400

    adding additional SQL practice

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -244,3 +244,51 @@ SELECT winner, subject
 FROM nobel
 WHERE yr=1984
 ORDER BY subject IN ('Physics', 'Chemistry'), subject, winner
+
+/*
+List each country name where the population is larger than that of 'Russia'.
+ */
+SELECT name FROM world
+WHERE population >
+(SELECT population FROM world
+WHERE name='Russia')
+
+/*
+Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
+ */
+SELECT name FROM world
+WHERE continent = 'Europe'
+AND gdp/population > 
+(SELECT gdp/population FROM world
+WHERE name = 'United Kingdom')  
+
+/*
+List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+ */
+SELECT name, continent FROM world
+WHERE continent IN
+(SELECT continent FROM world
+WHERE name IN ('Argentina', 'Australia'))
+ORDER BY name
+
+/*
+Which country has a population that is more than Canada but less than Poland? Show the name and the population.
+ */
+SELECT name, population FROM world
+WHERE population > 
+(SELECT population FROM world
+WHERE name = 'Canada')
+AND population <
+(SELECT population FROM world
+WHERE name = 'Poland')
+
+/*
+Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
+Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+The format should be Name, Percentage 
+ */
+SELECT name, CONCAT(ROUND((population /
+(SELECT population FROM world
+WHERE name = 'Germany') * 100)), '%') AS percentage
+FROM world
+WHERE continent = 'Europe'
