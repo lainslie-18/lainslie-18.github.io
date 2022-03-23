a813181af51138c9feae53affe908c13ebfe34b3
Lori Ainslie <lori.ainslie18@gmail.com>
2022-03-23 05:00:35 -0400

    updating with additional sql practice

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -355,3 +355,33 @@ How many countries have an area of at least 1000000
  */
 SELECT COUNT(name) FROM world
 WHERE area >= 1000000
+
+
+ /*
+What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+ */
+SELECT SUM(population) FROM world
+WHERE name IN ('Estonia', 'Latvia', 'Lithuania')
+
+ /*
+For each continent show the continent and number of countries.
+ */
+SELECT continent, COUNT(name) as num_countries
+FROM world
+GROUP BY continent
+
+ /*
+For each continent show the continent and number of countries with populations of at least 10 million.
+ */
+SELECT continent, COUNT(name) as num_countries
+FROM world
+WHERE population >= 10000000
+GROUP BY continent
+
+ /*
+List the continents that have a total population of at least 100 million.
+ */
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) >= 100000000
