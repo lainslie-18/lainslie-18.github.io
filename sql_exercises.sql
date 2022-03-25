69471c75cb6fd6bcedeb38990372014c245785b6
Lori Ainslie <lori.ainslie18@gmail.com>
2022-03-25 04:50:24 -0400

    updating with additional completed expercises

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -385,3 +385,37 @@ SELECT continent
 FROM world
 GROUP BY continent
 HAVING SUM(population) >= 100000000
+
+ /*
+Show the matchid and player name for all goals scored by Germany. 
+ */
+SELECT matchid, player FROM goal
+WHERE teamid = 'GER'
+
+/*
+Show id, stadium, team1, team2 for just game 1012.
+ */
+SELECT id,stadium,team1,team2
+FROM game
+WHERE id = 1012
+
+/*
+Show the player, teamid, stadium and mdate for every German goal.
+ */
+SELECT player,teamid, stadium, mdate
+FROM game JOIN goal ON (id=matchid)
+WHERE teamid = 'GER'
+
+/*
+Show the team1, team2 and player for every goal scored by a player called Mario.
+ */
+SELECT team1, team2, player
+FROM game JOIN goal ON (id=matchid)
+WHERE player LIKE 'Mario%'
+
+/*
+Show player, teamid, coach, gtime for all goals scored in the first 10 minutes.
+ */
+SELECT player, teamid, coach, gtime
+FROM goal JOIN eteam ON (teamid = id)
+WHERE gtime<=10
