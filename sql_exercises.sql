6db481fb2f0c95c69366e62d0bed174a57aa8065
Lori Ainslie <lori.ainslie18@gmail.com>
2022-03-30 14:27:44 -0400

    updating with additional completed sql practice

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -419,3 +419,22 @@ Show player, teamid, coach, gtime for all goals scored in the first 10 minutes.
 SELECT player, teamid, coach, gtime
 FROM goal JOIN eteam ON (teamid = id)
 WHERE gtime<=10
+
+
+
+/*
+Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+
+Equilateral: It's a triangle with 3 sides of equal length.
+Isosceles: It's a triangle with 2 sides of equal length.
+Scalene: It's a triangle with 3 sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle.
+ */
+SELECT 
+    CASE
+        WHEN NOT (A + B > C) AND (B + C > A) AND (C + A > B) THEN 'Not A Triangle'
+        WHEN A = B AND B = C THEN 'Equilateral'
+        WHEN A = B OR B = C OR C = A THEN 'Isosceles'
+        ELSE 'Scalene'
+    END AS triangle_types
+FROM TRIANGLES;
