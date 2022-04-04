2a65c9fb1617c5c88f5a6828c40cdcc1bb723e42
Lori Ainslie <lori.ainslie18@gmail.com>
2022-04-03 20:12:24 -0400

    updating with additional sql practice

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -471,3 +471,14 @@ Write a query calculating the amount of error (i.e.: actual - miscalculated  ave
  */
 SELECT ROUND(AVG(salary))  - ROUND(AVG(REPLACE(salary, '0', ''))) 
 FROM employees
+
+/*
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+ */
+SELECT FLOOR(AVG(population)) FROM city;
+
+/*
+Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+ */
+SELECT SUM(population) FROM city
+WHERE countrycode = 'JPN';
