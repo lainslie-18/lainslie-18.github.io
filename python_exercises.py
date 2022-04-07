55d1c2bac08d3d117aadf11d9dbb33ab0893bc9c
Lori Ainslie <lori.ainslie18@gmail.com>
2022-04-07 17:50:23 -0400

    updating with additional practice

--- python_exercises.py
+++ python_exercises.py
@@ -143,3 +143,22 @@ print(len(students))
 # What is the most common type of pet for web development students?
 # What is the average name length?
 # What is the highest pet age for light coffee drinkers?
+
+'''
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+'''
+if __name__ == '__main__':
+    n = int(input().strip())
+if n % 2 == 1:
+    print('Weird')
+elif n % 2 == 0 and n in range(2,6):
+    print('Not Weird')
+elif n % 2 == 0 and n in range(6, 21):
+    print('Weird')
+elif n % 2 == 0 and n > 20:
+    print('Not Weird')
