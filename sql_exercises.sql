b1e68abb429fd0c6567e2f17bedcaf8d1a5705f8
Lori Ainslie <lori.ainslie18@gmail.com>
2022-04-08 21:26:58 -0400

    updating with addtional sql practice

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -491,3 +491,17 @@ FROM employee
 GROUP BY total_earnings
 ORDER BY total_earnings DESC
 LIMIT 1;
+
+/*
+Query the following two values from the STATION table:
+1. The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+2. The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+ */
+SELECT ROUND(SUM(lat_n), 2), ROUND(SUM(long_w), 2)
+FROM station
+
+/*
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. Truncate your answer to  decimal places.
+*/
+SELECT ROUND(SUM(lat_n), 4) FROM station
+WHERE lat_n > 38.7880 AND lat_n < 137.2345
